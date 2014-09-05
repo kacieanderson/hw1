8ce04ad507e3b2f9d714d25e1922f5a897b75e7f
Kacie Anderson <kqanders@usc.edu>
2014-09-05 10:57:08 -0700

    Update hw1q4.cpp
    Added comments

--- hw1q4.cpp
+++ hw1q4.cpp
@@ -13,29 +13,29 @@ int main(int argc, char ** argv){
     string line;
     string word;
 
-    file >> numberOfLines;
+    file >> numberOfLines; // gets the number of lines to check from the 1st line in the file
     string lines[numberOfLines];
     int numWords[numberOfLines];
 
     getline(file, line); 
 
     for (int k = 0; k < numberOfLines; k++){
-      getline(file, line);
-      lines[k] = line;
+      getline(file, line); 
+      lines[k] = line; // fills an array with each individual line in the file
     }
 
     for (int i = 0; i < numberOfLines; i++){
-      stringstream stream(lines[i]);
+      stringstream stream(lines[i]); // looks at an individual line
       string word;
-      while(stream >> word){
-        ++numberOfWords;
+      while(stream >> word){ // while the sstream can get another word...
+        ++numberOfWords; // ...update the number of words gotten
       }
-      numWords[i] = numberOfWords;
-      numberOfWords = 0;
+      numWords[i] = numberOfWords; // fills an array with the number of words in each line
+      numberOfWords = 0; 
     }
 
     for (int c = numberOfLines - 1; c >= 0; c--){
-      cout << numWords[c] << endl;
+      cout << numWords[c] << endl; // outputs the number of words in each line, backwards.
     }
 
     return 0;
